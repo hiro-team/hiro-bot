58cc7dc9bf54ef59b63e0fe7059683f34e135569
bariscodefxy <bariscodefx@gmail.com>
2021-06-16 12:19:35 +0300

    database: Fix AddUser

--- src/database/Database.php
+++ src/database/Database.php
@@ -88,12 +88,14 @@ class Database extends PDO
             return false;
         }else {
             $query = $this->prepare("INSERT INTO users SET discord_id = :discord_id, money = :money, register_time = :register_time");
-            $query->execute([
+            if($query->execute([
                 "discord_id" => $data['discord_id'],
                 "money" => 0,
                 "register_time" => time()
-            ]);
-            return true;
+            ]))
+                return true;
+            else
+                return false;
         }
     }
 
