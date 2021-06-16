69b234505b86fd67f6192ce7b808217e27a2e8d5
bariscodefxy <bariscodefx@gmail.com>
2021-06-16 11:58:20 +0300

    bot: Money Command and Database

--- src/commands/Money.php
+++ src/commands/Money.php
@@ -5,40 +5,60 @@ namespace hiro\commands;
 use Discord\DiscordCommandClient;
 use Discord\Parts\Embed\Embed;
 use Discord\Parts\Embed\Field;
+use Dotenv\Dotenv;
+use hiro\CommandLoader;
+use hiro\database\Database;
 
 /**
- * Money command class
+ * Class Money
+ * @package hiro\commands
  */
 class Money
 {
 
     /**
-     * command $category
+     * @var string Command Category
      */
     private $category;
 
     /**
-     * $client
+     * @var DiscordCommandClient
      */
     private $discord;
 
     /**
-     * $loader
+     * @var Database
      */
-    private $loader;
+    private $database;
 
     /**
-     * __construct
+     * Money constructor.
+     * @param DiscordCommandClient $client
      */
     public function __construct(DiscordCommandClient $client)
     {
-        $this->loader = $loader;
         $this->category = "economy";
         $this->discord = $client;
+        include_once('../../db-settings.inc');
+        $this->database = new Database($db_host, $db_dbname, $db_user, $db_pass);
         $client->registerCommand('money', function($msg, $args)
         {
+            $user_money = $this->database->->getUserMoney($this->database->getUserIdByDiscordId($msg->author->id));
+            if(!$user_money)
+            {
+                if(!this->database->addUser([
+                    "discord_id" => $msg->author->id
+                ]))
+                {
+                    $embed = new Embed($this->discord);
+                    $embed->setTitle('You are couldnt added to database.');
+                    $msg->channel->sendEmbed($embed);
+                    return;
+                }else
+                    $user_money = 0;
+            }
             $embed = new Embed($this->discord);
-            $embed->setTitle('Your money: $0');
+            $embed->setTitle("Your money: $".$user_money);
             $embed->setTimestamp();
             $embed->setColor('#ff0000');
             $msg->channel->sendEmbed($embed);
@@ -50,6 +70,10 @@ class Money
         ]);
     }
 
+    /**
+     * @param string $name
+     * @return mixed
+     */
     public function __get(string $name)
     {
         return $this->{$name};
---
+++ src/database/Database.php
@@ -0,0 +1,100 @@
+<?php
+
+
+namespace hiro\database;
+
+use PDO;
+use PDOException;
+
+/**
+ * Class Database
+ * @package hiro\database
+ */
+class Database extends PDO
+{
+
+    /**
+     * Database constructor.
+     * @param string $host
+     * @param string $dbname
+     * @param string $user
+     * @param string $pass
+     * @param string|string $charset
+     */
+    public function __construct(string $host,
+                                string $dbname,
+                                string $user,
+                                string $pass,
+                                string $charset = "utf8"){
+        try {
+            parent::__construct("mysql:host=" . $host";dbname=" . $dbname . ";charset=" . $charset, $user, $pass)
+        }catch(PDOException $e)
+        {
+            echo $e . PHP_EOL;
+        }
+    }
+
+    /**
+     * @param int $user_id
+     * @return false|mixed
+     */
+    public function getUser(int $user_id)
+    {
+        $query = $this->prepare("SELECT * FROM users WHERE id = ?");
+        $query->execute([$user_id]);
+        if($query->rowCount())
+            return $query->fetch(PDO::FETCH_CLASS);
+        else
+            return false;
+    }
+
+    /**
+     * @param int $user_id
+     * @return false|mixed
+     */
+    public function getUserMoney(int $user_id)
+    {
+        if($this->getUser($user_id))
+        {
+            $user = $this->getUser($user_id);
+            return $user->money;
+        }else {
+            return false;
+        }
+    }
+
+    /**
+     * @param int $discord_id
+     * @return false|mixed
+     */
+    public function getUserIdByDiscordId(int $discord_id)
+    {
+        $query = $this->prepare("SELECT * FROM users WHERE discord_id = ?");
+        $query->execute([$discord_id]);
+        if($query->rowCount())
+            return $query->fetch(PDO::FETCH_ASSOC)->id;
+        else
+            return false;
+    }
+
+    /**
+     * @param array $data
+     * @return bool
+     */
+    public function addUser(array $data)
+    {
+        if(!$data['discord_id'])
+        {
+            return false;
+        }else {
+            $query = $db->prepare("INSERT INTO users SET discord_id = :discord_id, money = :money, register_time = :register_time");
+            $query->execute([
+                "discord_id" => $data['discord_id'],
+                "money" => 0,
+                "register_time" => time()
+            ]);
+            return true;
+        }
+    }
+
+}
