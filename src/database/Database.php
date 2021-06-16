a073dcdd63af22329da033a43f41cbf80ecaa1c4
bariscodefxy <bariscodefx@gmail.com>
2021-06-16 12:36:13 +0300

    test

--- src/database/Database.php
+++ src/database/Database.php
@@ -57,6 +57,7 @@ class Database extends PDO
         if($this->getUser($user_id))
         {
             $user = $this->getUser($user_id);
+            print_r($user);
             return $user['money'];
         }else {
             return false;
