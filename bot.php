f9662876c21c7073b648edbebbfb8facadf8abe6
bariscodefx <85716242+bariscodefxy@users.noreply.github.com>
2021-06-23 23:12:18 +0300

    bot: Presence Update

--- bot.php
+++ bot.php
@@ -19,7 +19,7 @@ $bot->on('ready', function($discord) {
     $commandLoader = new CommandLoader($discord);
     
     $act = $discord->factory(Activity::class, [
-        "name" => php_uname(),
+        "name" => explode(' ', php_uname())[0] . "•https://mobilpara.net/discord",
         "type" => Activity::TYPE_WATCHING
     ]);
     $discord->updatePresence($act, false, 'online');
