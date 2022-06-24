9a4b9fc6f47d0f188387e875e7f0d90f23926781
bariscodefxy <bariscodefx@gmail.com>
2022-06-24 23:34:23 +0300

    added WaifuNSFW command

---
+++ src/commands/WaifuNSFW.php
@@ -0,0 +1,103 @@
+<?php
+
+/**
+ * Copyright 2021 bariscodefx
+ * 
+ * This file part of project Hiro 016 Discord Bot.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+namespace hiro\commands;
+
+use Discord\DiscordCommandClient;
+use Discord\Parts\Embed\Embed;
+use hiro\interfaces\HiroInterface;
+use hiro\interfaces\CommandInterface;
+use Psr\Http\Message\ResponseInterface;
+use React\Http\Browser;
+
+/**
+ * WaifuNSFW command class
+ */
+class WaifuNSFW implements CommandInterface
+{
+    
+    /**
+     * command category
+     */
+    private $category;
+    
+    /**
+     * $client
+     */
+    private $discord;
+    
+    /**
+     * __construct
+     */
+    public function __construct(HiroInterface $client)
+    {
+        $this->discord = $client;
+        $this->category = "nsfw";
+        $this->browser = new Browser(null, $this->discord->getLoop());
+        $client->registerCommand('waifunsfw', function($msg, $args)
+        {
+            if(!$msg->channel->nsfw)
+            {
+                $msg->reply('You have to use this command in nsfw channel!');
+                return;
+            }
+            $type_array = [
+                "waifu",
+                "neko",
+                "trap",
+                "blowjob"
+            ];
+            if(!isset($args[0])) $type = "waifu";
+            if(isset($args[0])){
+                if(!in_array($args[0], $type_array))
+                {
+                    $msg->reply("{$args[0]} is not available. \nAvailable categories: `". implode(", ", $type_array) . "`");
+                    return;
+                }
+                $type = $args[0];
+            }
+            $this->browser->get("https://api.waifu.pics/nsfw/$type")->then(
+                function (ResponseInterface $response) use ($msg) {
+                    $result = (string) $response->getBody();
+                    $api = json_decode($result);
+                    $embed = new Embed($this->discord);
+                    $embed->setColor("#EB00EA");
+                    $embed->setTitle('Horny Waifu Generator');
+                    $embed->setDescription("{$msg->user->username} Your random horny waifu :)");
+                    $embed->setImage($api->url);
+                    $embed->setTimestamp();
+                    $msg->channel->sendEmbed($embed);
+                },
+                function (Exception $e) use ($msg) {
+                    $msg->reply('Unable to acesss the waifu.pics API :(');
+                }
+            );
+        },
+        [
+            "description" => "How much u are waifu"
+        ]);
+    }
+    
+    public function __get(string $name)
+    {
+        return $this->{$name};
+    }
+    
+}
