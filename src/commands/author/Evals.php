929b4b7a9c7de0229b2dfdf8c911a4b01c6ac274
bariscodefxy <bariscodefx@gmail.com>
2023-02-17 15:21:30 +0300

    [Evals] reply used in try catch

--- src/commands/author/Evals.php
+++ src/commands/author/Evals.php
@@ -68,9 +68,9 @@ class Evals extends Command
         }
         try {
             $output = eval($code);
+            $msg->reply($output);
         } catch (\Throwable $e) {
             $msg->reply("Error: \n```\n{$e->getMessage()}```");
         }
-        $msg->reply($output);
     }
 }
