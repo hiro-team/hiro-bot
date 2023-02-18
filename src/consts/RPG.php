366fac5619e8fc01d5f439c9c601fe2e29c1bc55
bariscodefxy <bariscodefx@gmail.com>
2023-02-18 19:55:54 +0300

    [RPG] update constants

--- src/consts/RPG.php
+++ src/consts/RPG.php
@@ -28,39 +28,58 @@ class RPG {
     /**
      * Character Types
      */
-    const UNSELECTED_CHAR                   = (0 << 0);
-    const WARRIOR_CHAR                      = (1 << 0);
-    const RANGER_CHAR                       = (1 << 1);
-    const MAGE_CHAR                         = (3 << 0);
-    const HEALER_CHAR                       = (2 << 1);
+    const WARRIOR_CHAR                      = (1 << 1);
+    const RANGER_CHAR                       = (1 << 2);
+    const MAGE_CHAR                         = (1 << 3);
+    const HEALER_CHAR                       = (1 << 4);
 
     /**
      * Race Types
      */
-    const HUMAN_RACE                        = (0 << 0);
-    const BEARHUMAN_RACE                    = (1 << 0);
-    const BEASTHUMAN_RACE                   = (1 << 1);
-    const DEERHUMAN_RACE                    = (3 << 0);
-    const DEMONBEAST_RACE                   = (2 << 1);
-    const DRAGON_RACE                       = (5 << 0);
-    const DRAGONKIN_RACE                    = (3 << 1);
-    const DWARF_RACE                        = (7 << 0);
-    const EARTHDRAGON_RACE                  = (4 << 1);
-    const ELF_RACE                          = (9 << 0);
-    const EVILEYETRIBE_RACE                 = (5 << 1);
-    const FOXHUMAN_RACE                     = (11 << 0);
-    const GIANT_RACE                        = (6 << 1);
-    const HALFONI_RACE                      = (13 << 0);
-    const HALFELF_RACE                      = (7 << 1);
-    const HYENAHUMAN_RACE                   = (15 << 0);
-    const LIZARDHUMAN_RACE                  = (8 << 1);
-    const ONI_RACE                          = (17 << 0);
-    const PHANTOM_RACE                      = (9 << 1);
-    const RABBITHUMAN_RACE                  = (19 << 0);
-    const SHUDRAK_RACE                      = (10 << 1);
-    const SNAKEHUMAN_RACE                   = (21 << 0);
-    const SPIRIT_RACE                       = (11 << 1);
-    const TANUKIHUMAN_RACE                  = (23 << 0);
-    const WOLFHUMAN_RACE                    = (12 << 1);
+    const HUMAN_RACE                        = (1 << 1);
+    const BEARHUMAN_RACE                    = (1 << 2);
+    const BEASTHUMAN_RACE                   = (1 << 3);
+    const DEERHUMAN_RACE                    = (1 << 4);
+    const DEMONBEAST_RACE                   = (1 << 5);
+    const DRAGON_RACE                       = (1 << 6);
+    const DRAGONKIN_RACE                    = (1 << 7);
+    const DWARF_RACE                        = (1 << 8);
+    const EARTHDRAGON_RACE                  = (1 << 9);
+    const ELF_RACE                          = (1 << 10);
+    const EVILEYETRIBE_RACE                 = (1 << 11);
+    const FOXHUMAN_RACE                     = (1 << 12);
+    const GIANT_RACE                        = (1 << 13);
+    const HALFONI_RACE                      = (1 << 14);
+    const HALFELF_RACE                      = (1 << 15);
+    const HYENAHUMAN_RACE                   = (1 << 16);
+    const LIZARDHUMAN_RACE                  = (1 << 17);
+    const ONI_RACE                          = (1 << 18);
+    const PHANTOM_RACE                      = (1 << 19);
+    const RABBITHUMAN_RACE                  = (1 << 20);
+    const SHUDRAK_RACE                      = (1 << 21);
+    const SNAKEHUMAN_RACE                   = (1 << 22);
+    const SPIRIT_RACE                       = (1 << 23);
+    const TANUKIHUMAN_RACE                  = (1 << 24);
+    const WOLFHUMAN_RACE                    = (1 << 25);
+
+    /**
+     * Gender Types
+     */
+    const MALE_GENDER                       = (1 << 1);
+    const FEMALE_GENDER                     = (1 << 2);
+
+    /**
+     * Weapons
+     */
+    const SWORD_WEAPON                      = 1;
+    const BOW_WEAPON                        = 2;
+    const STAFF_WEAPON                      = 3;
+    const MORNINGSTAR_WEAPON                = 4;
+
+    /**
+     * Weapon Types
+     */
+    const LONG_WEAPON                       = (1 << 1);
+    const SHORT_WEAPON                      = (1 << 2);
 
 }
