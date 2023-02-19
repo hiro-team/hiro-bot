09ccab04d38210d6211b1577a63a9f187bdd52d5
bariscodefxy <bariscodefx@gmail.com>
2023-02-19 23:48:19 +0300

    [bot] fix presence emote

--- bot.php
+++ bot.php
@@ -58,7 +58,7 @@ $bot->on('ready', function($discord) use ($shard_id, $shard_count) {
     ->setPresenceType(Activity::TYPE_WATCHING)
     ->setPresences([
         "{$_ENV['PREFIX']}help | " . $discord->formatNumber(sizeof($discord->guilds)) . " guilds | Shard " . $shard_id  + 1 . " of $shard_count",
-        ":crossed_swords: RPG System coming soon!"
+        "⚔️ RPG System coming soon!"
     ])
     ->startThread();
 
@@ -67,7 +67,7 @@ $bot->on('ready', function($discord) use ($shard_id, $shard_count) {
     {
         $presenceManager->setPresences([
             "{$_ENV['PREFIX']}help | " . $discord->formatNumber(sizeof($discord->guilds)) . " guilds | Shard " . $shard_id + 1 . " of $shard_count",
-            ":crossed_swords: RPG System coming soon!"
+            "⚔️ RPG System coming soon!"
         ]);
     });
 
