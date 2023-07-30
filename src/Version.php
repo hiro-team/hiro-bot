acd89a6be0101979df3bef740626f5c74a77ba9c
bariscodefx <bariscodefx@protonmail.com>
2023-07-30 19:23:50 +0300

    Revert "Release: v1.0.3"
    This reverts commit d3f75fff7cc3c815a7c36043466c33cdf896dbaa.

--- src/Version.php
+++ src/Version.php
@@ -28,11 +28,11 @@ class Version {
     /**
      * VERSION
      */
-    const VERSION = "v1.0.3";
+    const VERSION = "v1.0.2";
 
     /**
      * TYPE
      */
-    const TYPE = "production";
+    const TYPE = "development";
 
 }
